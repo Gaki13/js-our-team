848834ecf9a2087afcea210ff2631a103f45d751
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-13 17:50:46 +0200

    creo l'array con gli oggeti che mi servono per fare le card

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 // Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 // Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
+
+/*-----------------
+    MAIN
+-----------------*/
+// 1.Divido la card in tutte le info neccessarie
+const cards = [
+    {
+        Name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: src="img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        Name: "Angela Caroll",
+        role: "Chief Editor",
+        img: src="img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        Name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: src="img/angela-social-media-manager.jpg",
+    },
+];
