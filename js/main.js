a2c66026a2697f2d212dd89e2ec00f09a4eff2a5
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-13 16:04:38 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,2 @@
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
